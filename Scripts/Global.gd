c36b06d6f0e0c402fc4b442854438284dfd782d8
Gamnraa <ltaz1466@gmail.com>
2024-08-17 14:20:25 -0400

    Bumper detection functionality

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var balls = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -4,29 +4,40 @@ extends AnimatableBody2D
 
 var sides = ["L", "R"]
 var next_anim = 1
-var anims = ["Activate", "Deactivate"]
+var calls = ["play", "play_backwards"]
 
 func _ready():
-	$AnimationPlayer.play(anims[next_anim] + sides[side])
+	$AnimationPlayer.call_deferred(calls[next_anim], "Activate" + sides[side])
+
 
 
 func _process(delta):
 	pass
+	#for ball in Global.balls:
+	#	if $Area2D.overlaps_body(ball) and not $AnimationPlayer.is_playing():
+	#		$Timer.wait_time = randf_range(0.0, 0.34)
+	#		$Timer.start()
+	#		$AnimationPlayer.call_deferred(calls[next_anim], "Activate" + sides[side])
 
 
-func _on_area_2d_area_entered(area):
-	if area is RigidBody2D and not $AnimationPlayer.is_playing():
+func _on_area_2d_body_entered(body):
+	if body is RigidBody2D and not $AnimationPlayer.is_playing():
 		print("activate paddle")
-		$Timer.wait_time = randf_range(0.0, 1.0)
+		$Timer.wait_time = randf_range(0.0, 0.34)
 		$Timer.start()
 
 
 func _on_timer_timeout():
-	$AnimationPlayer.play(anims[next_anim] + sides[side])
+	$AnimationPlayer.call_deferred(calls[next_anim], "Activate" + sides[side])
+	#$AnimationPlayer.play(anims[next_anim] + sides[side])
 
 
 func _on_animation_player_animation_finished(anim_name):
-	if next_anim == 1:
-		next_anim = 0
-		$Timer.wait_time = randf_range(0.0, 2.0)
+	if next_anim == 0:
+		next_anim = 1
+		$Timer.wait_time = randf_range(0.0, 1.1)
 		$Timer.start()
+	else: next_anim = 0
+	
+
+
