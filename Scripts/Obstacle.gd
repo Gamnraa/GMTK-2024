7edb8bea1edd1b34a72bbd004ac3c650ced40dae
Gamnraa <ltaz1466@gmail.com>
2024-08-17 09:32:28 -0400

    Basic shader to control obstacle color

--- Scripts/Obstacle.gd
+++ Scripts/Obstacle.gd
@@ -1,11 +1,12 @@
 extends AnimatableBody2D
-
+#0 = red, 1 = green, 2 = blue, 3 = yellow
 @export var color: int
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if color not in range(4): color = 0
+	$Sprite.material.set_shader_parameter("color", color)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
