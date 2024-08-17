bba5ff5c8ac3f6d75738539dbd029cc4797d916f
Gamnraa <ltaz1466@gmail.com>
2024-08-17 18:20:29 -0400

    Enable sprite setting outside the scene

--- Scripts/Obstacle.gd
+++ Scripts/Obstacle.gd
@@ -1,10 +1,14 @@
 extends AnimatableBody2D
 #0 = red, 1 = green, 2 = blue, 3 = yellow
 @export var color: int
+@export_file var sprite
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if sprite != "" and sprite != null:
+		$Sprite.texture = load(sprite)
+		
 	if color not in range(4): color = 0
 	$Sprite.material.set_shader_parameter("color", color)
 
