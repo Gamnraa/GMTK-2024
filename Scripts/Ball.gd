c77d583498d12b2b9eaf89206a71e9dd5f1fff24
Gamnraa <ltaz1466@gmail.com>
2024-08-18 19:20:11 -0400

    Expanding objects now send the ball flying

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -10,6 +10,12 @@ func _ready():
 func _process(delta):
 	pass
 
+func _integrate_forces(state):
+	#print(state.get_velocity_at_local_position(position))
+	for body in get_colliding_bodies():
+		var anim = body.get_node("AnimationPlayer")
+		if is_instance_valid(anim) and anim.is_playing() and anim.current_animation == "Expand":
+			apply_impulse(2 * state.get_velocity_at_local_position(position))
 
 func launch(force):
 	freeze = false
@@ -17,3 +23,7 @@ func launch(force):
 	apply_impulse(force)
 	Global.is_waiting_for_ball = false
 	Global.balls_active = 1
+
+
+func _on_body_entered(body):
+	print(body)
