08fa3db00cc683dd6efa5febe6efdfe2e65f82e2
Gamnraa <ltaz1466@gmail.com>
2024-08-19 22:42:18 -0400

    Scoring system

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -24,13 +24,16 @@ func _integrate_forces(state):
 	if new_size != size: change_size()
 	#print(state.get_velocity_at_local_position(position))
 	for body in get_colliding_bodies():
-		
-		var anim = body.get_node_or_null("AnimationPlayer")
-		var swing = body.get_node_or_null("Swing")
-		if is_instance_valid(anim) and anim.is_playing() and anim.current_animation == "Expand":
-			apply_impulse(3 * state.get_velocity_at_local_position(position))
-		elif is_instance_valid(swing) and anim.is_playing(): apply_impulse(2 * state.get_velocity_at_local_position(position))
-		
+		if not body in get_tree().get_nodes_in_group("wall"):
+			var score = 10
+			var anim = body.get_node_or_null("AnimationPlayer")
+			var swing = body.get_node_or_null("Swing")
+			if is_instance_valid(anim) and anim.is_playing() and anim.current_animation == "Expand":
+				apply_impulse(3 * state.get_velocity_at_local_position(position))
+				score = 30
+			elif is_instance_valid(swing) and anim.is_playing(): apply_impulse(2 * state.get_velocity_at_local_position(position))
+			Global.score += score
+			
 
 func launch(force):
 	freeze = false
